8dfa8e328c781278e48bef19851bc7e53affc31f
robopsychology <128649265+robopsychology@users.noreply.github.com>
2023-03-22 17:46:59 -0700

    Create home.nix

---
+++ home.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.config.allowUnfree = true;
+
+  home.username = "eddy";
+  home.homeDirectory = "/Users/eddy";
+
+  home.stateVersion = "22.11";
+
+  programs = {
+    vscode = {
+      enable = true;
+      package = pkgs.vscode;
+    };
+    git = {
+      enable = true;
+      userName = "robopsychology";
+    };
+  };
+
+  home.packages = [
+  ];
+
+  home.file = {
+  };
+
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  programs.home-manager.enable = true;
+}
